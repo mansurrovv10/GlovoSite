207534ab25ca51c6272d28c14460db690217ade2
mansurrovv10 <mansurovtilek99@gmail.com>
2025-12-14 21:27:28 +0600

    I added docker

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -138,6 +138,7 @@ MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
